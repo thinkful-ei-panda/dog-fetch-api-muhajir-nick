f09bf068ea54266284ca05dc0ec85e20a70c7531
M-Sayer <haji.san@icloud.com>
2020-06-09 12:53:46 -0400

    finished assignment

--- index.js
+++ index.js
@@ -3,18 +3,46 @@
 function watchForm() {
   $('form').submit((event) => {
     event.preventDefault();
-    const userInput = $("input[type=number]").val();
+    const userInput = $("input[type=text]").val();
     console.log(userInput);
     getDogImage(userInput);
   });
 }
 
 //this is fetch
-function getDogImage(num) {
-  fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
+function getDogImage(breed) {
+  // fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
+  fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+
     .then(response => response.json())
     .then(responseJson =>
-      console.log(responseJson));
+      // console.log(responseJson));
+      // generateRandomImages(responseJson));
+      generateRandomByBreed(responseJson))
+    .catch(error => alert('dog breed not found'));
 }
+
+
+function generateRandomByBreed(responseJson) {
+  let imageUrl = responseJson.message;
+  let imageHtml = `<img src="${imageUrl}" alt="Random Dog Image">`;
+
+  $('.images').html(imageHtml);
+}
+
+// Generates random dog images into DOM
+// function generateRandomImages(responseJson) {
+//   let imageHtml = ``;
+//   responseJson.message.forEach(imageURL => {
+//   imageHtml += `
+//   <section>
+//     <img src="${imageURL}" alt="Random Dog Image">
+//   </section>`;
+//   });
+//   $('.images').html(imageHtml);
+// }
+
+
+//render function
 
 $(watchForm);
